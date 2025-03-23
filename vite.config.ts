43d2ecddd6d509c953da9a57f2b1cecad00dd4a9
Cefriandy Simarmata <cefriandysimarmata@gmail.com>
2025-03-23 21:52:39 +0700

    rollback

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-cef/'
+  base: '/portfolio-cef/',
+  build: {
+    outDir: 'dist',
+  },
 })
