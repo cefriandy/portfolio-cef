1063349b9e03c675820c3f7158d28ad8279682ef
Cefriandy Simarmata <33171242+cefriandy@users.noreply.github.com>
2026-06-25 00:49:35 +0700

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-cef/',
+  base: '/',
   build: {
     outDir: 'dist',
     rollupOptions: {
